5a09ff7a0f576d170b9341f5d25b8842249f709a
kaoutar-12 <kmouradi@student.1337.ma>
2025-07-09 23:01:18 +0100

    add responsive

--- app/page.tsx
+++ app/page.tsx
@@ -165,10 +165,10 @@ export default function Home() {
           <span className="loader"></span>
         </div>
       ) : (
-        <div className="flex h-screen flex-col  items-center bg-[linear-gradient(to_right,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)]">
-          <div className="flex pl-[20px] w-full pr-[20px] justify-between items-center h-20">
-            <div className="w-[100px] h-8 "></div>
-            <div className="w-[900px] h-8 bg-[#444444] rounded-2xl pl-5 flex items-center gap-2">
+        <div className="flex min-h-screen flex-col items-center bg-[linear-gradient(to_right,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)]">
+          <div className="flex flex-col md:flex-row px-5 justify-between items-center h-auto md:h-20 w-full gap-4 md:gap-0 py-4 md:py-0">
+            <div className="w-full md:w-[100px] h-8" />
+            <div className="w-full md:w-[900px] h-10 md:h-8 bg-[#444444] rounded-2xl pl-5 flex items-center gap-2">
               <FaSearch className="inline-block text-white" />
               <form onSubmit={onSubmit} className="w-full h-full">
                 <input
@@ -176,49 +176,57 @@ export default function Home() {
                   value={search}
                   type="text"
                   placeholder="search"
-                  className="w-full h-full outline-none rounded-2xl border-none text-white"
+                  className="w-full h-full outline-none rounded-2xl border-none text-white bg-transparent"
                 />
               </form>
             </div>
-            <div className="w-[200px] h-8 bg-[#041BB1] font-extrabold text-white rounded-2xl flex items-center justify-center gap-2">
+            <div className="w-full md:w-[200px] h-10 md:h-8 bg-[#041BB1] font-extrabold text-white rounded-2xl flex items-center justify-center gap-2">
               <button className="flex items-center gap-2" onClick={getLocation}>
                 <span>Current Location</span>
                 <FaMapLocationDot className="text-black" />
               </button>
             </div>
           </div>
-          <div className="w-full h-full  flex flex-col items-center justify-center gap-[30px]">
-            <div className="w-[90%] h-[45%]  flex gap-[30px]">
-              <div className="bg-[linear-gradient(to_right_top,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)] w-1/3 h-full rounded-[30px]  shadow-[20px_0_15px_-6px,0_20px_10px_-6px] shadow-[#111111] flex items-center text-white justify-center flex-col gap-10">
+
+          <div className="w-full flex flex-col items-center justify-center gap-8 md:gap-[30px]">
+            {/* Top Section */}
+            <div className="w-[95%] flex flex-col md:flex-row gap-8 md:gap-[30px]">
+              {/* Left Card */}
+              <div className="bg-[linear-gradient(to_right_top,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)] w-full md:w-1/3 h-auto rounded-[30px] shadow-[20px_0_15px_-6px,0_20px_10px_-6px] shadow-[#111111] flex items-center text-white justify-center flex-col gap-10 p-5">
                 <div className="text-[26px] font-bold">
                   {data?.name || "Rabat"}
                 </div>
                 <div className="flex items-center justify-center flex-col">
-                  <div className="font-bold text-[96px]">
+                  <div className="font-bold text-[56px] md:text-[96px]">
                     {getLocalTime(data!.dt, data!.timezone)}
                   </div>
-                  <div className="text-[20px]">Thursday, 31 Aug</div>
+                  <div className="text-[18px] md:text-[20px]">
+                    Thursday, 31 Aug
+                  </div>
                 </div>
               </div>
-              <div className="bg-[radial-gradient(circle,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)] shadow-[20px_0_15px_-6px,0_20px_10px_-6px] shadow-[#111111] rounded-4xl w-2/3 h-full flex text-white ">
-                <div className="w-1/3 h-full flex flex-col gap-10 items-center justify-center">
+
+              {/* Right Card */}
+              <div className="bg-[radial-gradient(circle,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)] shadow-[20px_0_15px_-6px,0_20px_10px_-6px] shadow-[#111111] rounded-4xl w-full md:w-2/3 h-auto flex flex-col md:flex-row text-white">
+                {/* Column 1 */}
+                <div className="w-full md:w-1/3 flex flex-col gap-10 items-center justify-center p-4">
                   <div className="flex flex-col items-center justify-center">
-                    <span className="text-[80px] font-extrabold font-poppins bg-gradient-to-r from-white to-[#5e5e5e] bg-clip-text text-transparent">
+                    <span className="text-[40px] md:text-[80px] font-extrabold font-poppins bg-gradient-to-r from-white to-[#5e5e5e] bg-clip-text text-transparent">
                       {convertTemp(data!.main.temp)}°C
-                    </span>{" "}
+                    </span>
                     <div className="flex items-center gap-2 bg-gradient-to-r from-[#e4e4e4] to-[#c2c2c2] bg-clip-text text-transparent">
-                      <span className="text-[20px] font-extrabold">
+                      <span className="text-[16px] md:text-[20px] font-extrabold">
                         Feels Like:
-                      </span>{" "}
-                      <span className="text-[32px] font-extrabold">
+                      </span>
+                      <span className="text-[20px] md:text-[32px] font-extrabold">
                         {convertTemp(data!.main.feels_like)}°C
                       </span>
                     </div>
                   </div>
                   <div className="flex flex-col gap-5 items-center justify-center">
                     <div className="flex items-center justify-center gap-5">
-                      <BsSunrise className="w-[58px] h-[58px]" />
-                      <div className="flex flex-col items-center justify-center text-[20px] font-semibold">
+                      <BsSunrise className="w-[40px] md:w-[58px] h-[40px] md:h-[58px]" />
+                      <div className="flex flex-col items-center justify-center text-[16px] md:text-[20px] font-semibold">
                         <span>Sunrise</span>
                         <span>
                           {getLocalTime(data!.sys.sunrise, data!.timezone)}
@@ -226,8 +234,8 @@ export default function Home() {
                       </div>
                     </div>
                     <div className="flex items-center justify-center gap-5">
-                      <BsSunset className="w-[58px] h-[58px]" />
-                      <div className="flex flex-col items-center justify-center text-[20px] font-semibold">
+                      <BsSunset className="w-[40px] md:w-[58px] h-[40px] md:h-[58px]" />
+                      <div className="flex flex-col items-center justify-center text-[16px] md:text-[20px] font-semibold">
                         <span>Sunset</span>
                         <span>
                           {getLocalTime(data!.sys.sunset, data!.timezone)}
@@ -236,66 +244,64 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div className="w-1/3 h-full flex items-center justify-center flex-col">
+
+                {/* Column 2 */}
+                <div className="w-full md:w-1/3 flex items-center justify-center flex-col p-4">
                   <Image
                     src={`https://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}
                     alt="Weather Icon"
-                    width={260}
-                    height={260}
+                    width={160}
+                    height={160}
                   />
-                  <span className="text-[32px] font-bold text-white">
+                  <span className="text-[24px] md:text-[32px] font-bold text-white">
                     {data?.weather[0].main}
                   </span>
                 </div>
-                <div className="w-1/3 h-full ">
-                  <div className="w-full h-1/2 flex">
-                    <div className="w-1/2 h-full gap-5 flex justify-center items-center flex-col">
-                      <div className="flex items-center justify-center gap-2 flex-col">
-                        <Image
-                          src="/humidity.svg"
-                          width={100}
-                          height={100}
-                          alt="Weather Icon"
-                        />
-                        <span className="text-[20px] font-bold">
-                          {data?.main.humidity}%
-                        </span>
-                      </div>
-                      <span className="text-[16px] font-semibold">
+
+                {/* Column 3 */}
+                <div className="w-full md:w-1/3 p-4">
+                  <div className="flex flex-wrap md:flex-nowrap">
+                    <div className="w-1/2 flex flex-col items-center justify-center gap-2">
+                      <Image
+                        src="/humidity.svg"
+                        width={60}
+                        height={60}
+                        alt="Humidity"
+                      />
+                      <span className="text-[18px] md:text-[20px] font-bold">
+                        {data?.main.humidity}%
+                      </span>
+                      <span className="text-[14px] md:text-[16px] font-semibold">
                         Humidity
                       </span>
                     </div>
-                    <div className="w-1/2 h-full flex gap-5 justify-center items-center flex-col">
-                      <div className="flex items-center justify-center gap-2 flex-col">
-                        <Image
-                          src="/wind.svg"
-                          width={100}
-                          height={100}
-                          alt="Weather Icon"
-                        />
-                        <span className="text-[20px] font-bold">
-                          {data!.wind.speed} m/s
-                        </span>
-                      </div>
-                      <span className="text-[16px] font-semibold">
+                    <div className="w-1/2 flex flex-col items-center justify-center gap-2">
+                      <Image
+                        src="/wind.svg"
+                        width={60}
+                        height={60}
+                        alt="Wind"
+                      />
+                      <span className="text-[18px] md:text-[20px] font-bold">
+                        {data!.wind.speed} m/s
+                      </span>
+                      <span className="text-[14px] md:text-[16px] font-semibold">
                         Wind Speed
                       </span>
                     </div>
                   </div>
-                  <div className="w-full h-1/2  flex justify-center items-center">
-                    <div className="w-1/2 h-full gap-5 flex justify-center items-center flex-col">
-                      <div className="flex items-center justify-center gap-2 flex-col">
-                        <Image
-                          src="/pressure.svg"
-                          width={100}
-                          height={100}
-                          alt="Weather Icon"
-                        />
-                        <span className="text-[20px] font-bold">
-                          {data?.main.pressure} hPa
-                        </span>
-                      </div>
-                      <span className="text-[16px] font-semibold">
+                  <div className="flex justify-center mt-4">
+                    <div className="flex flex-col items-center justify-center gap-2">
+                      <Image
+                        src="/pressure.svg"
+                        width={60}
+                        height={60}
+                        alt="Pressure"
+                      />
+                      <span className="text-[18px] md:text-[20px] font-bold">
+                        {data?.main.pressure} hPa
+                      </span>
+                      <span className="text-[14px] md:text-[16px] font-semibold">
                         Pressure
                       </span>
                     </div>
@@ -303,18 +309,17 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="w-[90%] h-[45%] flex gap-[10px]">
-              <div className="bg-[linear-gradient(to_right_bottom,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)] shadow-[20px_0_15px_-6px,0_20px_10px_-6px] shadow-[#111111] w-full h-full flex flex-col gap-[10px] rounded-4xl justify-center items-center">
-                <span className="text-[32px] font-extrabold text-white">
+
+            {/* Hourly Forecast */}
+            <div className="w-[95%] flex flex-col gap-4">
+              <div className="bg-[linear-gradient(to_right_bottom,#0b061d,#121329,#171b37,#1b2445,#1f2d53,#233865,#264377,#274e8a,#285ea6,#266ec3,#1d7fe0,#0091ff)] shadow-[20px_0_15px_-6px,0_20px_10px_-6px] shadow-[#111111] w-full flex flex-col gap-[10px] rounded-4xl justify-center items-center p-4">
+                <span className="text-[24px] md:text-[32px] font-extrabold text-white">
                   Hourly Forecast:
                 </span>
-                <div className="flex gap-[10px] ">
+                <div className="flex overflow-x-auto gap-[10px] w-full justify-start md:justify-center">
                   {forcast.map((item, index) => {
-                    // Extract the timestamp for the forecast item
                     const forecastTimestamp =
                       new Date(item.dt_txt).getTime() / 1000;
-
-                    // Check if the forecast time is between sunrise and sunset
                     const isDaytime =
                       data?.sys.sunrise && data?.sys.sunset
                         ? forecastTimestamp >= data.sys.sunrise &&
@@ -324,13 +329,13 @@ export default function Home() {
                     return (
                       <div
                         key={index}
-                        className={`w-[230px] h-[330px] rounded-[50px] justify-center items-center text-white pt-5 ${
+                        className={`min-w-[180px] md:min-w-[230px] h-[330px] rounded-[30px] justify-center items-center text-white pt-5 flex flex-col ${
                           isDaytime
                             ? "bg-[linear-gradient(to_bottom,#e19d00,#e1a233,#e1a84f,#dfad68,#dcb37f)]"
                             : "bg-[linear-gradient(to_bottom,#000000,#1d1219,#2c1d32,#2e2e51,#0c4272)]"
                         }`}
                       >
-                        <div className="flex items-center justify-center text-[32px] font-bold">
+                        <div className="flex items-center justify-center text-[20px] md:text-[32px] font-bold">
                           {data?.timezone !== undefined
                             ? getLocalTime(
                                 new Date(item?.dt_txt).getTime() / 1000,
@@ -338,18 +343,17 @@ export default function Home() {
                               )
                             : "Timezone not available"}
                         </div>
-                        <div className="flex items-center justify-center flex-col text-[26px] font-bold">
+                        <div className="flex items-center justify-center flex-col text-[20px] md:text-[26px] font-bold">
                           <Image
                             src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
                             alt="Weather Icon"
                             width={100}
                             height={100}
-                            className="w-[100px] h-[100px]"
                           />
                           {convertTemp(item.main.temp)}°C
                         </div>
-                        <div className="flex items-center justify-center flex-col text-[26px] font-bold">
-                          <FaWind className="w-[60px] h-[60px]" />
+                        <div className="flex items-center justify-center flex-col text-[20px] md:text-[26px] font-bold">
+                          <FaWind className="w-[40px] md:w-[60px] h-[40px] md:h-[60px]" />
                           {item.wind.speed} m/s
                         </div>
                       </div>
